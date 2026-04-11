cf74b54ae677dcc272fedb75e72a32c6aeb4df96
Trần Thị Thúy Na <letthithuyna11.42021@gmail.com>
2026-04-12 02:24:41 +0700

    quanlycv

--- he_thong_quan_ly_van_ban/he_thong_quan_ly_van_ban/urls.py
+++ he_thong_quan_ly_van_ban/he_thong_quan_ly_van_ban/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -11,3 +13,6 @@ urlpatterns = [
     path("", include(("apps.quanlycongviec.urls", "quanlycongviec"), namespace="quanlycongviec")),
     path("", include(("apps.hosovanban.urls", "hosovanban"), namespace="hosovanban")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
