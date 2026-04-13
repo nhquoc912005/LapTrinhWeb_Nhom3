fc2e60a5d3a44b5027d9a30049963be3790fc638
PhamPhuong2005 <phamphuong20032005@gmail.com>
2026-04-13 09:58:24 +0700

    cap nhat

--- he_thong_quan_ly_van_ban/apps/hosovanban/urls.py
+++ he_thong_quan_ly_van_ban/apps/hosovanban/urls.py
@@ -6,5 +6,9 @@ from .views import danh_sach
 app_name = "hosovanban"
 
 urlpatterns = [
-    path("ho-so-van-ban.html", danh_sach, name="danh_sach"),
+    path("hosovanban/ho-so-van-ban.html", danh_sach, name="danh_sach"),
+    path("hosovanban/them-ho-so-van-ban.html", them_ho_so_van_ban, name="them_ho_so_van_ban"),
+    path("hosovanban/chi-tiet/<int:pk>/", chi_tiet_ho_so_van_ban, name="chi_tiet"),
+    path("hosovanban/sua/<int:pk>/", sua_ho_so_van_ban, name="sua"),
+    path("hosovanban/xoa/<int:pk>/", xoa_ho_so_van_ban, name="xoa"),
 ]
